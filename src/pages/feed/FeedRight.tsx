49e105db25d9732b6c520550d5b80d6dcfa7ab04
Thash <afthashsahal123@gmail.com>
2026-09-06 07:40:50 +0930

    minor fixes

--- src/pages/feed/FeedRight.tsx
+++ src/pages/feed/FeedRight.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { useAppSelector } from '@/app/hooks';
 import Notifications from './Notifications';
+import { Bell, ChevronDown } from 'lucide-react';
 
 export default function FeedRight() {
   const [dropDownOpen, setDropDownOpen] = useState(false);
@@ -17,17 +16,16 @@ export default function FeedRight() {
           className="flex justify-center items-center w-full"
           onClick={() => setDropDownOpen(!dropDownOpen)}
         >
-          <h1 className="flex justify-center items-center my-3 text-xl font-semibold">
+          <h1 className="flex justify-center items-center my-3 text-xl font-semibold gap-1">
             <div className="max-w-0 overflow-hidden transition-all duration-1000 ease-in-out group-hover:max-w-50">
               <span className="mr-1 opacity-0 transition-opacity duration-1000 group-hover:opacity-100">
                 Notifications
               </span>
             </div>
-            <span>
-              <FontAwesomeIcon icon={faBell} />
-            </span>
+            
+            <Bell className="h-5 w-5" />
             <div className="flex justify-center items-center h-5 w-5 rounded-full bg-secondary p-3">
-              <span className={`text-white ${count < 10 ? 'text-sm' : 'text-xs'} font-bold`}>
+              <span className={`text-white ${count < 10 ? 'text-sm' : 'text-xs'}`}>
                 {count}
               </span>
             </div>
@@ -36,7 +34,7 @@ export default function FeedRight() {
                 !dropDownOpen && 'duration-300 group-hover:scale-125 group-hover:rotate-12'
               } ${dropDownOpen ? 'rotate-180' : ''}`}
             >
-              <FontAwesomeIcon icon={faChevronDown} />
+              <ChevronDown className="h-5 w-5" />
             </span>
           </h1>
         </div>
